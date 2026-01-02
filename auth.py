dd3385e0dc72fc26861b80d167c818ed018ed347
Michael Kinder <michaelk046@protonmail.com>
2026-01-02 08:13:42 -0500

    add AsyncSession

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
